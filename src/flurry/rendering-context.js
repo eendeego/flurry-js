903a334479bfc327ebba326249d5b6416f9f6581
Luis Reis <luis.m.reis@gmail.com>
2020-04-18 02:12:07 -0700

    Fix missing flow types in rendering-context.js

--- src/flurry/rendering-context.js
+++ src/flurry/rendering-context.js
@@ -1,5 +1,13 @@
+/* flurry */
+
+// @flow
+
+const nullthrows = require('nullthrows');
+
 export type FlurryRenderingContext = {|
   gl: WebGLRenderingContext,
+  width: number,
+  height: number,
 |};
 
 function initExtension(gl: WebGLRenderingContext, extension: string): void {
@@ -12,15 +20,14 @@ function initExtension(gl: WebGLRenderingContext, extension: string): void {
 export function newRenderingContext(
   canvas: HTMLCanvasElement,
 ): FlurryRenderingContext {
-  const gl = canvas.getContext('webgl');
-  // const gl = null;
+  const gl = nullthrows(canvas.getContext('webgl'));
 
   initExtension(gl, 'OES_element_index_uint');
   initExtension(gl, 'OES_texture_float');
 
   return {
+    gl,
     width: canvas.width,
     height: canvas.height,
-    gl,
   };
 }
